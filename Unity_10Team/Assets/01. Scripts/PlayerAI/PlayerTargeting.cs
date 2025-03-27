7012381866dfb877a8895b183bd1813562cc662d
JUNG99 <kinggodjung9@naver.com>
2025-03-27 11:56:03 +0900

    [feat] 플레이어 타겟팅 스크립트
    - 적을 감지하는 기능
    - +@

---
+++ Unity_10Team/Assets/01. Scripts/PlayerAI/PlayerTargeting.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTargeting : MonoBehaviour
+{
+    public float searchRadius = 10f; //탐지반경
+    public LayerMask enemyLayer; //적레이어
+    public GameObject currentTarget; //현재타겟
+
+    private void Update()
+    {
+        currentTarget = FindNearestEnemy();
+    }
+
+    //적찾기
+
+    GameObject FindNearestEnemy()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, searchRadius, enemyLayer);
+        GameObject closest = null;
+        float minDistance = Mathf.Infinity;
+        Vector3 currentPostion = transform.position;
+
+        foreach(Collider collider in colliders)
+        {
+            float distance = Vector3.Distance(currentPostion, collider.transform.position);
+            
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closest = collider.gameObject;
+
+            }
+        }
+
+        return closest;
+    }
+
+}
