5b866655713fef91ac72594b29b31d79a1a6ce1c
Kim-giseok <belbeck@naver.com>
2025-03-31 20:25:32 +0900

    [Feat] 게임종료 ui 갱신 임시 구현

--- Unity_10Team/Assets/01. Scripts/UI/IngameUI/EndPopUpUI.cs
+++ Unity_10Team/Assets/01. Scripts/UI/IngameUI/EndPopUpUI.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class EndPopUpUI : BaseUI
 {
+    [SerializeField] TextMeshProUGUI resultTxt;
     public override void Init()
     {
         UiType = UITYPE.GAMEOVER;
         base.Init();
+    }
+
+    protected override void UpdateUI()
+    {
+        base.UpdateUI();
+
+        int playTime = (int)(Time.time - GameManager.Instance.gameStartTime);
+        string playTimeTxt = $"{playTime / 60:D2}:{playTime % 60:D2}";
+
+        resultTxt.text = playTimeTxt + "\n";
+    }
+
+    private void OnEnable()
+    {
+        UpdateUI();
     }
 }
