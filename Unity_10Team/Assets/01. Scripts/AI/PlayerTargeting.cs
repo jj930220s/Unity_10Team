04be7c40eb8f17bf65151ae729c4c7bb336079fb
JUNG99 <kinggodjung9@naver.com>
2025-03-31 14:56:52 +0900

    [feat] 플레이어의 적 타겟팅 추가
    - 가장 가까운 적을 찾았을 때, 플레이어와 적 사이에 장애물이 있다면 다시 탐색

--- Unity_10Team/Assets/01. Scripts/AI/PlayerTargeting.cs
+++ Unity_10Team/Assets/01. Scripts/AI/PlayerTargeting.cs
@@ -6,41 +6,51 @@ public class PlayerTargeting : MonoBehaviour
 {
     public float searchRadius = 10f; //탐지반경
     public LayerMask enemyLayer; //적레이어
+    public LayerMask obstacleLayer; //장애물 레이어
     public GameObject currentTarget; //현재타겟
 
     private void Update()
     {
-        currentTarget = FindNearestEnemy();
-        if (currentTarget != null )
+        currentTarget = FindNearestVisibleEnemy();
+        if (currentTarget != null)
         {
-            Debug.Log("적탐지");
+            Debug.Log("적 탐지");
         }
     }
 
-    //적찾기
-
-    GameObject FindNearestEnemy()
+    //가장 가까운 적을 찾는데, 장애물에 안가려진 것 우선
+    GameObject FindNearestVisibleEnemy()
     {
         Collider[] colliders = Physics.OverlapSphere(transform.position, searchRadius, enemyLayer);
-        GameObject closest = null;
+        List<GameObject> visibleEnemies = new List<GameObject>();
         float minDistance = Mathf.Infinity;
-        Vector3 currentPostion = transform.position;
+        GameObject closest = null;
+        Vector3 currentPosition = transform.position;
 
-        foreach(Collider collider in colliders)
+        foreach (Collider collider in colliders)
         {
-            float distance = Vector3.Distance(currentPostion, collider.transform.position);
-            
+            Vector3 direction = (collider.transform.position - currentPosition).normalized;
+            float distance = Vector3.Distance(currentPosition, collider.transform.position);
+
+            if (!Physics.Raycast(currentPosition, direction, distance, obstacleLayer))
+            {
+                visibleEnemies.Add(collider.gameObject);
+            }
+        }
+
+        //가장 가까운 적 찾기
+        foreach (GameObject enemy in visibleEnemies)
+        {
+            float distance = Vector3.Distance(currentPosition, enemy.transform.position);
             if (distance < minDistance)
             {
                 minDistance = distance;
-                closest = collider.gameObject;
-
+                closest = enemy;
             }
         }
 
         return closest;
     }
-
 
     private void OnDrawGizmos()
     {
